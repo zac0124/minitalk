57043ea282b83e57fcc8af5001f8911fab1d299a
Zac0124 <mzorigt91@gmail.com>
2022-07-05 15:54:56 +0930

    added libraries

--- client.c
+++ client.c
@@ -10,4 +10,53 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include "ft_printf.h"
+#include "libft.h"
 
+void    ft_send_bit(int pid, char *str, size_t len)
+{
+    int shift;
+    size_t index;
+
+    index = 0;
+    while(index <= len)
+    {
+        shift = 0;
+        while(shift < 7)
+        {
+            if((str[index] >> shift) & 1)
+            {
+                kill(pid, SIGUSR2);
+            }
+            else
+            {
+                kill(pid, SIGUSR1);
+            }
+            shift++;
+            usleep(300);
+        }
+        index++;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int pid;
+    char    *str;
+
+    if(argc == 3)
+    {
+        pid = ft_atoi(argv[1]);
+        str = argv[2];
+        ft_send_bit(pid, str, ft_strlen(str));
+    }
+    else
+    {
+        ft_printf("\n ERROR");
+    }
+    
+}
--- server.c
+++ server.c
@@ -10,4 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include "ft_printf.h"
 
+typedef struct server_message
+{
+    char    c;
+    size_t  index;
+}   text_message;
+text_message    g_msg;
+
+void    ft_bit_handler(int  bit)
+{
+    int index;
+
+    index = 0;
+    g_msg.c += ((bit & 1) << g_msg.index);
+
+
+}
